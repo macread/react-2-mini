8d8b91923927aaf3619b023579fea7edd9eb769b
Mac Read <james.macgregor.read@gmail.com>
2018-05-14 13:46:54 -0600

    lunch

--- instructor-code/src/App.js
+++ instructor-code/src/App.js
@@ -9,43 +9,37 @@ import TextContainer from './components/TextContainer';
 
 class App extends Component {
   // constructor
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       fontColor: 'black',
-      fontSiset: 12,
+      fontSize: 12,
       fontFamily: 'monospace',
       allowEdit: 'true'
     }
-
-    this.updateColor = this.updateColor.bind(this)
-    this.updateSize = this.updateSize.bind(this)
-    this.updateFamily = this.updateFamily.bind(this)
-    this.updateEditStatus = this.updateEditStatus.bind(this)
+    this.updateColor = this.updateColor.bind(this);
+    this.updateSize = this.updateSize.bind(this);
+    this.updateFamily = this.updateFamily.bind(this);
+    this.updateEditStatus = this.updateEditStatus.bind(this);
   }
-
   // updateColor
-
   updateColor(val) {
     this.setState({
-      fontColor: val
+      fontColor: val   
     })
   }
-
   // updateSize
   updateSize(val) {
     this.setState({
       fontSize: val
     })
   }
-
   // updateFamily
   updateFamily(val) {
     this.setState({
       fontFamily: val
     })
   }
-
   // updateEditStatus
   updateEditStatus(val) {
     this.setState({
@@ -58,9 +52,9 @@ class App extends Component {
       <div>
         <div className="headerBar">
           { /* Render EditToggle */ }
-          <EditToggle update={this.updateEditStatus}/>
+          <EditToggle update={this.updateEditStatus} />
           { /* Render ColorChanger */ }
-          <ColorChanger update={this.updateColor} allowEdit={this.state.allowEdit}/>
+          <ColorChanger update={this.updateColor} allowEdit={this.state.allowEdit} />
           { /* Render SizeChanger */ }
           <SizeChanger update={this.updateSize} allowEdit={this.state.allowEdit} />
           { /* Render FamilyChanger */ }
--- instructor-code/src/components/ColorChanger.js
+++ instructor-code/src/components/ColorChanger.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function ColorChanger(props) {
+  console.log(props)
   return (
-    <select className="dropDownContainer"
-    onChange={(e)=> props.update(e.target.value)}
+    <select 
+      className="dropDownContainer" 
+      onChange={(e)=> props.update(e.target.value)}
     >
       <option value="black"> Black </option>
       <option value="blue"> Blue </option>
